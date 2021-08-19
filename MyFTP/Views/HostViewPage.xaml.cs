ee797f6573060b7d1e1494a9b4f22917c264a174
Luandersonn Airton <luandersonn@outlook.com>
2021-08-19 00:22:44 -0300

    Fix TreeView selection bug and Host Info button (#16)
    * Add settings page (#14)
    
    * Add initial setting page
    
    * Update login view to use FtpHostSettings
    
    * Add HostViewModel singleton on Services
    
    * Add email buttonRemove password from vault on delete FTPHostSettings
    
    * Add keyboard shortcuts
    
    * Add More Keyboard Accelerators
    
    * Add login dialog
    
    * Bugfix: New Connection dialog does not use the app theme
    
    * Update to fix treeview selection bug
    
    * Fix treeview selection error
    
    * Show host info button

--- MyFTP/Views/HostViewPage.xaml.cs
+++ MyFTP/Views/HostViewPage.xaml.cs
@@ -39,7 +39,7 @@ namespace MyFTP.Views
 				WeakReferenceMessenger.Default.Register<SelectedItemChangedMessage<FtpListItemViewModel>>(this, OnSelectedItemChanged);
 				_onTreeViewSelectedItemChangedToken = treeView.RegisterPropertyChangedCallback(muxc.TreeView.SelectedItemProperty, OnSelectedItemChanged);
 				Window.Current.CoreWindow.PointerPressed += OnCoreWindowPointerPressed;
-				
+
 				this.AddKeyboardAccelerator(VirtualKey.Back, OnAcceleratorRequested);
 				this.AddKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu, OnAcceleratorRequested);
 				this.AddKeyboardAccelerator(VirtualKey.Right, VirtualKeyModifiers.Menu, OnAcceleratorRequested);
@@ -165,12 +165,24 @@ namespace MyFTP.Views
 			if (args.CurrentPoint.Properties.IsXButton1Pressed)
 			{
 				// Mouse back button pressed
-				args.Handled = NavigationHistory.GoBack();
+				if (NavigationHistory.GoBack())
+				{
+					args.Handled = true;
+				}
+
 			}
 			else if (args.CurrentPoint.Properties.IsXButton2Pressed)
 			{
 				// Mouse forward button pressed				
 				args.Handled = NavigationHistory.GoForward();
+			}
+			if (args.Handled)
+			{
+				// Due TreeView bug, you need set the node manually :(
+				if (NavigationHistory.CurrentItem != null && NavigationHistory.CurrentItem.Parent == null) // Root item!
+				{
+					treeView.SelectedNode = treeView.RootNodes.FirstOrDefault(x => x.Content == NavigationHistory.CurrentItem);
+				}
 			}
 		}
 
