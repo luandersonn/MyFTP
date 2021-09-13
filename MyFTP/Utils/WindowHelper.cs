5f0775d1f2f6ed98fad18d1c071e7f497ca6f346
Luandersonn Airton <luandersonn@outlook.com>
2021-09-12 22:07:55 -0300

    Add properties window

---
+++ MyFTP/Utils/WindowHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Metadata;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace MyFTP.Utils
+{
+	public static class WindowHelper
+	{
+		public static bool IsAppWindowSupported => ApiInformation.IsTypePresent("Windows.UI.WindowManagement.AppWindow");
+
+		public static async Task<AppWindow> OpenInNewAppWindowAsync<T>(
+					object parameter,
+					AppWindowPresentationKind presentationKind = AppWindowPresentationKind.Default,
+					string title = null)
+			where T : Page
+		{
+			if (!IsAppWindowSupported)
+				throw new NotSupportedException();
+
+			// Create the AppWindow
+			AppWindow appWindow = await AppWindow.TryCreateAsync();
+
+			// Create the main frame and navigate for the page
+			var appWindowContentFrame = new Frame();
+			appWindowContentFrame.Navigate(typeof(T), parameter);
+
+			// Set the AppWindow content
+			ElementCompositionPreview.SetAppWindowContent(appWindow, appWindowContentFrame);
+
+			// Resize
+			var windowSize = new Size(500, 360);
+			appWindow.RequestSize(windowSize);
+
+			// Placement
+			var windowPlacement = CalculateWindowPlacement(windowSize);
+			appWindow.RequestMoveRelativeToCurrentViewContent(windowPlacement);
+
+			// Set title
+			if (title != null)
+				appWindow.Title = title;
+
+			// Set the presentation mode
+			var result = appWindow.Presenter.RequestPresentation(presentationKind);
+
+			// Try show the app Window
+			result = await appWindow.TryShowAsync();
+
+			if (result)
+			{
+				// Handle the close window event
+				appWindow.Closed += (s, e) =>
+				{
+					appWindowContentFrame.Content = null;
+					appWindow = null;
+				};
+				// return the result
+				return appWindow;
+			}
+			return null;
+		}
+
+		/// <summary>
+		/// Calculate the AppWindow offset based on current window
+		/// </summary>
+		/// <param name="appWindowSize"></param>
+		/// <returns></returns>
+		private static Point CalculateWindowPlacement(Size appWindowSize)
+		{
+			var windowWidth = Window.Current.Bounds.Width;
+			var windowHeight = Window.Current.Bounds.Height;
+			var x = (windowWidth / 2) - (appWindowSize.Width / 2);
+			var y = (windowHeight / 2) - (appWindowSize.Height / 2);
+			return new Point(x, y);
+		}
+	}
+}
---
+++ MyFTP/Views/ItemPropertiesViewPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using FluentFTP;
+using MyFTP.ViewModels;
+using System;
+using System.IO;
+using Windows.Storage.FileProperties;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace MyFTP.Views
+{
+	public sealed partial class ItemPropertiesViewPage : Page
+	{
+		public FtpListItemViewModel ViewModel { get => (FtpListItemViewModel)GetValue(ViewModelProperty); set => SetValue(ViewModelProperty, value); }
+		public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register("ViewModel",
+			typeof(FtpListItemViewModel), typeof(ItemPropertiesViewPage), new PropertyMetadata(null, ItemChanged));
+
+		public ItemPropertiesViewPage()
+		{
+			InitializeComponent();
+			Services.ISettings settings = (Services.ISettings)App.Current.Services.GetService(typeof(Services.ISettings));
+			ElementTheme theme = ElementTheme.Default;
+			settings?.TryGet("AppTheme", out theme);
+			RequestedTheme = theme;
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e) => ViewModel = e.Parameter as FtpListItemViewModel;
+
+		private static async void ItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var page = (ItemPropertiesViewPage)d;
+			if (e.NewValue is FtpListItemViewModel m)
+			{
+				uint size = 80;
+				StorageItemThumbnail thumbnail;
+				var extension = Path.GetExtension(m.Name);
+				if (m.Type == FluentFTP.FtpFileSystemObjectType.Directory)
+				{
+					thumbnail = await Utils.IconHelper.GetFolderIconAsync(size);
+				}
+				else
+				{
+					thumbnail = await Utils.IconHelper.GetFileIconAsync(extension, size);
+				}
+				thumbnail.Seek(0);
+				await page.image.SetSourceAsync(thumbnail);
+				page.contentTypeTextBlock.Text = await Utils.IconHelper.GetContentType(extension);
+			}
+		}
+
+		private bool CanRead(FtpPermission permission) => (permission & FtpPermission.Read) == FtpPermission.Read;
+		private bool CanWrite(FtpPermission permission) => (permission & FtpPermission.Write) == FtpPermission.Write;
+		private bool CanExecute(FtpPermission permission) => (permission & FtpPermission.Execute) == FtpPermission.Execute;
+	}
+}
